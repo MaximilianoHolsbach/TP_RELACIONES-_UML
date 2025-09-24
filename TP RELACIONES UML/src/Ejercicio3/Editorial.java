31ede907fde5f8b449069b03f5df6d38f0bfd9f1
MaxiCode <andresholsbach@gmail.com>
2025-09-23 23:56:27 -0300

    Se finaliza ejercicio numero 3

--- TP RELACIONES UML/src/Ejercicio3/Editorial.java
+++ TP RELACIONES UML/src/Ejercicio3/Editorial.java
@@ -1,4 +1,16 @@
 package Ejercicio3;
 public class Editorial {
+    private String nombre;
+    private String direccion;
+    //Constructor
+    public Editorial(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+    //Metodos
+    public String getNombre() {
+        return nombre;
+    }
     
 }
+
--- TP RELACIONES UML/src/Ejercicio3/Libro.java
+++ TP RELACIONES UML/src/Ejercicio3/Libro.java
@@ -1,4 +1,23 @@
 package Ejercicio3;
 public class Libro {
+    private String titulo;
+    private String isbn;
+    private Autor autor;         // Asociación unidireccional
+    private Editorial editorial; // Agregación
+    //Constructor
+    public Libro(String titulo, String isbn, Autor autor, Editorial editorial) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.autor = autor;
+        this.editorial = editorial;
+    }
+    //Metodos
+    public void getAutor_del_Libro() {
+        System.out.println("El autor del libro: "+this.titulo+", es: "+autor.getNombre());
+    }
+    public void getEditorial_del_Libro() {
+        System.out.println("La editorial del libro: "+this.titulo+", es: "+editorial.getNombre());
+    }
     
 }
+
