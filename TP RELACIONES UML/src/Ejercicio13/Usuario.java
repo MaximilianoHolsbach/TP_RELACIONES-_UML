a49e0270f16ac549675738c6a9290bcfd099ded9
MaxiCode <andresholsbach@gmail.com>
2025-09-26 22:36:27 -0300

    Se finaliza ejercicio numero 13

---
+++ TP RELACIONES UML/src/Ejercicio13/Usuario.java
@@ -0,0 +1,17 @@
+package Ejercicio13;
+public class Usuario {
+    private String nombre;
+    private String email;
+    //Constructor
+    public Usuario(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+    //Metodos
+    public String getNombre() {
+        return nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
